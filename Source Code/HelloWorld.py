703374fdf14a166eef79876315389cf08b899491
Nonthawat Duangchai <nonthawat2538@hotmail.com>
2017-01-08 12:19:23 +0700

    HelloWorld

---
+++ Source Code/HelloWorld.py
@@ -0,0 +1,33 @@
+"""
+variable = [1,2,3,4,5,6,7.8]
+variable.append(109)
+variable.append(299)
+print(int(sum(variable)))
+"""
+
+"""
+age = 14
+print("I am {} years old".format(age))
+"""
+
+"""
+score = 79
+if score >= 80:
+    print("A")
+    print("You're very good")
+elif score>=70:
+    print("B")
+    print("You're good")
+elif score>=60:
+    print("C")
+    print("You're OK")
+elif score>=50:
+    print("D")
+    print("Do more to be better")
+else:
+    print("F")
+    print("Work hard and try again...")
+"""
+
+myMap = {"A" : "Very Good", "B" : "Good", "C" : "OK", "D" : "Bad", "F" : "Fail"}
+print(myMap['A'])
